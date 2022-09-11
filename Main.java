bdab98c4208955fd9f0823b9283a302dae203a18
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2022-09-11 19:17:09 -0400

    Make 2048 gameboard
    We makin' 2048 now

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 public class Main
 {
     public static boolean gameOn = true;
+    public static int[][] gameBoard = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
 
     public static void main (String[] args)
     {
@@ -12,20 +13,48 @@ public class Main
 
         clearCLI();
         while (gameOn) {
+            System.out.print(" > ");
             char myChar = keyboard.next().charAt(0);
             clearCLI();
             tick(myChar);
+            System.out.println(boardToStr());
         }
 
     }
 
     public static void tick (char uInput)
     {
-        System.out.println(gameOn);
     }
 
     public static void clearCLI ()
     {
         System.out.println("\n".repeat(69));
+    }
+
+    public static String boardToStr ()
+    {
+        String outStr = "";
+        for (int[] row : gameBoard) {
+            for (int cell : row) {
+                if (cell < 1) {
+                    outStr += "[    ]";
+                }
+                else if (cell < 10) {
+                    outStr += "[  " + cell + " ]";
+                }
+                else if (cell < 100) {
+                    outStr += "[ " + cell + " ]";
+                }
+                else if (cell < 1000) {
+                    outStr += "[ " + cell + "]";
+                }
+                else {
+                    outStr += "[" + cell + "]";
+                }
+            }
+            outStr += "\n";
+        }
+        outStr += "\b";
+        return outStr;
     }
 }
